634dbc6bc7a4988ae4bc39db6527295170944598
Dan Ambrogio <dan.ambrogio@gmail.com>
2020-01-24 18:03:04 -0500

    fetches all data for a given card from api.magicthegathering.io

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 #[macro_use]
 extern crate clap;
-use clap::App;
+extern crate surf;
 
-fn main() {
+use clap::App;
+use async_std::task;
+
+fn main() -> Result<(), surf::Exception> {
   let yaml = load_yaml!("../cli.yml");
-  let _matches = App::from_yaml(yaml).get_matches();
+  let matches = App::from_yaml(yaml).get_matches();
+
+  let card_name = matches.value_of("CARD").unwrap();
+
+  task::block_on(get_card(card_name))
+}
+
+async fn get_card(name: &str) -> Result<(), surf::Exception> {
+  let url = format!("https://api.magicthegathering.io/v1/cards?name={}", name);
+  let body: String = surf::get(url).recv_string().await?;
+
+  println!("{}", body);
+
+  Ok(())
 }
